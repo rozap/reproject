e7e78b38e1e55d26ea9976b818d8ee98cad93eaa
rozap <chrisd1891@gmail.com>
2026-06-22 11:37:40 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Reproject.Mixfile do
   def project do
     [
       app: :reproject,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: ">= 1.17.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
